b6f0711a114df8b7bf4d18b5577d89cd23f19e74
Vegar Gorseth <veggo@trondelagfylke.no>
2026-08-07 13:39:32 +0200

    nye gruppelister

--- js/groups.js
+++ js/groups.js
@@ -11,24 +11,44 @@
 
 export const GROUPS = [
   {
-    id: "res-sondag-8-11-gruppe-1",
-    name: "RES søndag 8-11 år gruppe 1",
+    id: "fredag-fodt-2010-2013",
+    name: "Fredag født 2010-2013",
+    password: "fredag1"
+  },
+  {
+    id: "fredag-fodt-2014-2017",
+    name: "Fredag født 2014-2017",
+    password: "fredag2"
+  },
+  {
+    id: "sondag-counter-strike-2",
+    name: "Søndag Counter Strike 2",
+    password: "sondagcs2"
+  },
+  {
+    id: "sondag-gr1-fodt-2014-2017",
+    name: "Søndag gr 1 født 2014-2017",
     password: "sondag1"
   },
   {
-    id: "res-sondag-8-11-gruppe-2",
-    name: "RES søndag 8-11 år gruppe 2",
+    id: "sondag-gr2-fodt-2014-2017",
+    name: "Søndag gr 2 født 2014-2017",
     password: "sondag2"
   },
   {
-    id: "res-torsdag-gruppe-1",
-    name: "RES torsdag gruppe 1",
+    id: "teknisk-og-trenere",
+    name: "Teknisk og trenere",
+    password: "teknisk"
+  },
+  {
+    id: "torsdag-fodt-2010-2013",
+    name: "Torsdag født 2010-2013",
     password: "torsdag1"
   },
   {
-    id: "res-fredag-gruppe-1",
-    name: "RES fredag gruppe 1",
-    password: "fredag1"
+    id: "torsdag-fodt-2014-2017",
+    name: "Torsdag født 2014-2017",
+    password: "torsdag2"
   }
   // Legg til flere grupper her etter samme mønster:
   // {
